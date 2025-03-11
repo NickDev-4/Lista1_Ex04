652b05924d49f33bbd0add17f51515ebb69d01d3
NickDev-4 <nickolasfreire.santos@gmail.com>
2025-03-11 12:58:55 -0300

    Exercicio_4

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.White;
+
+Console.WriteLine("I'm The Slime");
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Black;
+
+Console.WriteLine("");
+
+Console.WriteLine("I am gross and perverted\nI'm obsessed and deranged\nI have existed for years\nBut very little has changed\nI'm the tool of the Government\nAnd industry too\nFor I am destined to rule\nAnd regulate you");
+
+Console.WriteLine("");
+
+Console.WriteLine("I may be vile and pernicious\nBut you can't look away\nI make you think I'm delicious\nWith the stuff that I say\nI'm the best you can get\nHave you guessed me yet?\nI'm the slime oozin' out\nFrom your TV set");
+
+Console.WriteLine("");
+
+Console.WriteLine("You will obey me while I lead you\nAnd eat the garbage that I feed you\nUntil the day that we don't need you\nDon't go for help, no one will heed you\nYour mind is totally controlled\nIt has been stuffed into my mold\nAnd you will do as you are told\nUntil the rights to you are sold");
+
+Console.WriteLine("");
+
+Console.WriteLine("That's right, folks\nDon't touch that dial");
+
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine("Well, I am the slime from your video");
+
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("Oozin' along on your livin' room floor");
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine("I am the slime from your video");
+
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("Can't stop the slime, people, look at me go");
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine("I am the slime from your video");
+
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("Oozin' along on your livin' room floor");
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine("I am the slime from your video");
+
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("Can't stop the slime, people, look at me go");
